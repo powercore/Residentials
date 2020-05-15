362c708f81e385f414d8a6cf185062ba5034d55f
powercore <shimyakov@gmail.com>
2020-05-16 00:54:40 +0500

    1

--- Residentials/Residentials.cs
+++ Residentials/Residentials.cs
@@ -21,24 +21,25 @@ namespace Residentials
       public string Owner { get; set; }
       public string Address { get; set; }
       public List<ResidentialsRecord> changes = new List<ResidentialsRecord>();
+      public List<ResidentialsRecord> registered = new List<ResidentialsRecord>();
 
-    public Residentials CreateResidentionals(string buildingDate)
-    {
-      Residentials residentials = new Residentials
+      public Residentials CreateResidentionals(string buildingDate)
       {
-        BuildingDate = buildingDate
-      };
-      return residentials;
-    }
+        Residentials residentials = new Residentials
+        {
+          BuildingDate = buildingDate
+        };
+        return residentials;
+      }
 
-    public Residentials ChangeOwner(Residentials residentials, string newOwner)
-    {
-      residentials.Owner = newOwner;
-      ResidentialsRecord rr = new ResidentialsRecord { recordValue = $"New owner is {newOwner}", recordDate = DateTime.Now };
-      residentials.changes.Add(rr);
+      public Residentials ChangeOwner(Residentials residentials, string newOwner)
+      {
+        residentials.Owner = newOwner;
+        ResidentialsRecord rr = new ResidentialsRecord { recordValue = $"New owner is {newOwner}", recordDate = DateTime.Now };
+        residentials.changes.Add(rr);
 
-      return residentials;
-    }
+        return residentials;
+      }
 
   }
 }
